bf4861416b1623b367746318523f22a2074b140f
Andrew Murray <radarhere@users.noreply.github.com>
2021-08-16 19:11:22 +1000

    Added Python 3.10.0rc1

--- build_install_pythons.sh
+++ build_install_pythons.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Install Pythons 2.7 3.4 3.5 3.6 3.7 3.8 3.9 and matching pips
+# Install Pythons 2.7 3.4 3.5 3.6 3.7 3.8 3.9 3.10 and matching pips
 set -ex
 
 echo "deb http://ppa.launchpad.net/deadsnakes/ppa/ubuntu trusty main" > /etc/apt/sources.list.d/deadsnakes.list
@@ -84,6 +84,7 @@ build_openssl 1.0.2o
 compile_python 3.7.6 "--with-openssl=/usr/local/ssl"
 compile_python 3.8.1 "--with-openssl=/usr/local/ssl"
 compile_python 3.9.0 "--with-openssl=/usr/local/ssl"
+compile_python 3.10.0rc1 "--with-openssl=/usr/local/ssl"
 
 # Install certificates from certifi, for Python 3.7
 # Thanks to Github user Mr BitBucket for this fix.
